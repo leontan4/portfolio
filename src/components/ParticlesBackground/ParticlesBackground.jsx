31bedaeafe2b62775f4de3832b24e9ec1ddaee3e
Leon Tan <tanxx519@umn.edu>
2021-04-25 18:30:30 -0500

    Update ParticlesBackground.jsx

--- src/components/ParticlesBackground/ParticlesBackground.jsx
+++ src/components/ParticlesBackground/ParticlesBackground.jsx
@@ -3,11 +3,7 @@ import Particles from "react-particles-js";
 import ParticleConfig from "../../config/particle-config";
 
 const ParticlesBackground = () => {
-<<<<<<< HEAD
 	return <Particles className="particles" params={ParticleConfig} />;
-=======
-	return <Particles height="99vh" width="100vw" params={ParticleConfig} />;
->>>>>>> 088ea45d2e2828de9cbf64976397cc9acedc7a15
 };
 
 export default ParticlesBackground;
